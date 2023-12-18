0582f98f86e568d13963336dff210f5c9b19ad9c
Sulamaxx <115876513+Sulamaxx@users.noreply.github.com>
2023-12-18 16:04:27 +0530

    a

---
+++ AddContact.js
@@ -0,0 +1,83 @@
+import { Button, TextInput, View } from "react-native";
+
+const AddContact = () => {
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [mobile1, setMobile1] = useState('');
+    const [mobile2, setMobile2] = useState('');
+    const [email, setEmail] = useState('');
+
+    return (
+        <View style={styles.inputContainer}>
+            <TextInput
+                style={styles.input}
+                placeholder="First Name"
+                value={firstName}
+                onChangeText={(text) => setFirstName(text)}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Last Name"
+                value={lastName}
+                onChangeText={(text) => setLastName(text)}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Mobile 1"
+                value={mobile1}
+                onChangeText={(text) => setMobile1(text)}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Mobile 2"
+                value={mobile2}
+                onChangeText={(text) => setMobile2(text)}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Email"
+                value={email}
+                onChangeText={(text) => setEmail(text)}
+            />
+            <Button title="Add Contact" onPress={addContact} />
+        </View>
+    )
+}
+
+export default AddContact;
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    inputContainer: {
+        marginBottom: 20,
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 10,
+        paddingHorizontal: 10,
+    },
+    contactItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+        width: '100%',
+    },
+});
